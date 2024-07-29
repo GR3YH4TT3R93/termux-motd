e99321b86aaa999e9d8b8875167ecd6b50d83f1c
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-07-29 02:24:46 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 
-git clone https://github.com/Generator/termux-motd.git /data/data/com.termux/files/usr/etc/motd
+git clone https://github.com/GR3YH4TT3R93/termux-motd.git /data/data/com.termux/files/usr/etc/motd
 
 [ $(basename $SHELL) == "zsh" ] && _PROFILE=/data/data/com.termux/files/usr/zprofile || _PROFILE=/data/data/com.termux/files/usr/etc/profile
 [ $(grep "/data/data/com.termux/files/usr/etc/motd/init.sh" $_PROFILE) != "0" ] && echo "/data/data/com.termux/files/usr/etc/motd/init.sh" >> $_PROFILE
